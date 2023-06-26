574ce69755a16cf46c7acd2fbac7389e650ee2fe
Eli Allen <65967638+mojojoflow@users.noreply.github.com>
2023-06-25 20:13:36 -0500

    Complete exercise #4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,5 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...removal) {
+    return array.filter(x=> !removal.includes(x));
 };
 
 // Do not edit below this line
