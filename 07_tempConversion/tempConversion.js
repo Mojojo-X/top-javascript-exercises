9a08e4846f78f4311504bbeca405a5b35f1ad0ff
Eli Allen <65967638+mojojoflow@users.noreply.github.com>
2023-06-26 21:06:47 -0500

    Complete exercise #7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fah) {
+  temp = ((fah - 32) * 5/9).toFixed(1);
+  return +temp;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cel) {
+  temp = (cel * 9/5 + 32).toFixed(1);
+  return +temp;
 };
 
 // Do not edit below this line
