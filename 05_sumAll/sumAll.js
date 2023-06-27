fb22356bb4dd6e3e0dda6066918338ae7d0777e6
Eli Allen <65967638+mojojoflow@users.noreply.github.com>
2023-06-26 20:18:09 -0500

    Complete exercise #5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = function (start, end) {
+  if (
+    !Number.isInteger(start) ||
+    !Number.isInteger(end) ||
+    start < 0 ||
+    end < 0
+  ) {
+    return "ERROR";
+  }
 
+  sum = 0;
+  if (start > end) {
+    for (let i = start; i >= end; i--) {
+      sum += i;
+    }
+    return sum;
+  } else {
+    for (let i = start; i <= end; i++) {
+      sum += i;
+    }
+    if (sum === 0) return "ERROR";
+    return sum;
+  }
 };
 
 // Do not edit below this line
