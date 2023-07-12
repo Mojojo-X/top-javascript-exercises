90e7ee527b5bb11929cdfc963d9c72ab307a5c4c
Eli Allen <65967638+mojojoflow@users.noreply.github.com>
2023-07-11 21:02:45 -0500

    Complete exercise #9
    TheOdinProject's Foundations Course: JS Fundamentals part 5 assignment

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  const original = str.match(/\w/g);
+  const reverse = original.slice().reverse();
+  return original.join('').toLowerCase() === reverse.join('').toLowerCase();
 };
 
 // Do not edit below this line
