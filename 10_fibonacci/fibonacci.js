c662175008ea516b046c8aa80921725861d7e332
Eli Allen <65967638+mojojoflow@users.noreply.github.com>
2023-07-11 21:28:47 -0500

    Complete exercise #10
    TheOdinProject's Foundations Course: JS Fundamentals part 5 assignment

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,10 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+  if (num < 0) return "OOPS";
+  const start = [1,1]
+  for (let i = 2; i < num; i++) {
+    start.push(start.at(-1) + start.at(-2));
+  }
+  return start.at(-1);
 };
 
 // Do not edit below this line
