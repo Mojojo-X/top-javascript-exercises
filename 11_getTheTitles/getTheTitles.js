27f89064408c0b80f76a8963e8fa232f8cd5a3a7
Eli Allen <65967638+mojojoflow@users.noreply.github.com>
2023-07-12 21:26:05 -0500

    Complete exercise # 11
    TheOdinProject's Foundations Course: JS Fundamentals part 5 assignment

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(obj) {
+  const books = []
+  for (let book of obj) {
+    books.push(book.title)
+  }
+  return books;
 };
 
 // Do not edit below this line
